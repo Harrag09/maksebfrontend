67d68d758bb2c6e2a85c9bf3b7d2846ad3bef754
Harrag09 <Harragoussama10@gmail.com>
2024-01-23 17:28:32 +0100

    Merge changes from remote repository

--- src/service/LogoutService.js
+++ src/service/LogoutService.js
@@ -1,10 +1,6 @@
 import axios from "axios";
-<<<<<<< HEAD
 const API_BASE_URL = "https://makseb.onrender.com/";
 
-=======
-const API_BASE_URL = "/";
->>>>>>> 74210047b9894ec647565ca525be40d429da9a48
 const LogoutService = {
   signout: async () => {
     try {
--- src/service/Register.js
+++ src/service/Register.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
 const API_BASE_URL = "https://makseb.onrender.com/";
-=======
-const API_BASE_URL = "/"; // Replace with your actual API URL
->>>>>>> 74210047b9894ec647565ca525be40d429da9a48
 
 export const signup = async (userData) => {
   try {
